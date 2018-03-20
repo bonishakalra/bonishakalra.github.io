c89f3ecb72d98d1b3c22d2858cb5e62a68eabe3e
bonishakalra <37516663+bonishakalra@users.noreply.github.com>
2018-03-20 20:44:13 +0530

    map correction2

--- shared/js/profile.js
+++ shared/js/profile.js
@@ -152,9 +152,9 @@ $('#skill-set').append(skillAppendHTML);
     }		*/
 
 
-function initMap() {
+/*function initMap() {
 	
-	/*var broadway = {
+	var broadway = {
 		info: '<strong>Chipotle on Broadway</strong><br>\
 					5224 N Broadway St<br> Chicago, IL 60640<br>\
 					<a href="https://goo.gl/maps/jKNEDz4SyyH2">Get Directions</a>',
@@ -182,7 +182,7 @@ function initMap() {
       [broadway.info, broadway.lat, broadway.long, 0],
       [belmont.info, belmont.lat, belmont.long, 1],
       [sheridan.info, sheridan.lat, sheridan.long, 2],
-    ];*/
+    ];
 
     var locations = [
       ['Hinjewadi, Pune', 18.516726, 73.856255],
@@ -211,6 +211,56 @@ function initMap() {
 			}
 		})(marker, i));
 	}
+}*/
+
+    function loadScript(src,callback){
+  
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    if(callback)script.onload=callback;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    script.src = src;
+  }
+  var locations = [
+      ['Hinjewadi, Pune', 18.516726, 73.856255],
+      ['Noida, UttarPradesh', 28.535517,	77.391029]];
+  
+  loadScript('http://maps.googleapis.com/maps/api/js?v=3&sensor=false&callback=initialize',
+              function(){log('google-loader has been loaded, but not the maps-API ');});
+
+
+function initialize() {
+    
+    log('maps-API has been loaded, ready to use');
+    var mapOptions = {
+          zoom: 4,
+          center: new google.maps.LatLng(18.516726, 73.856255),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+
+    var infowindow = new google.maps.InfoWindow({});
+
+	var marker, i;
+
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		});
+
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i][0]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
+  }
+
+function log(str){
+  document.getElementsByTagName('pre')[0].appendChild(document.createTextNode('['+new Date().getTime()+']\n'+str+'\n\n'));
 }
 
 
